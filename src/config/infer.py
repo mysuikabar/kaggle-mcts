ea2056a6608fa234ca71923d80503df72f52c985
mysuikabar <mizuho.suikabar@gmail.com>
2024-10-03 14:14:41 +0000

    feat: update infer script

--- src/config/infer.py
+++ src/config/infer.py
@@ -1,40 +1,10 @@
-import os
 from dataclasses import dataclass
 from pathlib import Path
-
-from consts import REPO_ROOT
-
-DATASET = "dataset_name"
 
 
 @dataclass
 class Config:
-    processor_path: Path
-    model_dir: Path
+    dataset_dir: Path
     test_path: Path
     submission_path: Path
     evaluation_api_path: Path | None = None
-
-
-_config_local_env = Config(
-    processor_path=REPO_ROOT / f"outputs/{DATASET}/processor.pickle",
-    model_dir=REPO_ROOT / f"outputs/{DATASET}/models",
-    test_path=REPO_ROOT / "data/raw/test.csv",
-    submission_path=REPO_ROOT / "data/raw/sample_submission.csv",
-    evaluation_api_path=REPO_ROOT / "data/raw",
-)
-
-_config_kaggle_env = Config(
-    processor_path=Path(f"/kaggle/input/{DATASET}/processor.pickle"),
-    model_dir=Path(f"/kaggle/input/{DATASET}/models"),
-    test_path=Path("/kaggle/input/um-game-playing-strength-of-mcts-variants/test.csv"),
-    submission_path=Path(
-        "/kaggle/input/um-game-playing-strength-of-mcts-variants/sample_submission.csv"
-    ),
-)
-
-
-if os.getenv("LOCAL_ENVIRONMENT"):
-    config = _config_local_env
-else:
-    config = _config_kaggle_env
--- src/infer.py
+++ src/infer.py
@@ -1,28 +1,60 @@
+import glob
 import os
 import sys
+from pathlib import Path
 
+import numpy as np
 import polars as pl
 
-from config.infer import config
-from ml.cv import load_models, predict_models
+from config.infer import Config
+from consts import REPO_ROOT
+from ml.model.factory import ModelFactory
 from process.process import Preprocessor, postprocess
 
-if os.getenv("LOCAL_ENVIRONMENT"):
-    sys.path.append(str(config.evaluation_api_path))
+DATASET = "run_name"  # change here
 
-import kaggle_evaluation.mcts_inference_server  # type: ignore
+config_local_env = Config(
+    dataset_dir=REPO_ROOT / f"outputs/{DATASET}",
+    test_path=REPO_ROOT / "data/raw/test.csv",
+    submission_path=REPO_ROOT / "data/raw/sample_submission.csv",
+    evaluation_api_path=REPO_ROOT / "data/raw",
+)
+
+config_kaggle_env = Config(
+    dataset_dir=Path(f"/kaggle/input/{DATASET}"),
+    test_path=Path("/kaggle/input/um-game-playing-strength-of-mcts-variants/test.csv"),
+    submission_path=Path(
+        "/kaggle/input/um-game-playing-strength-of-mcts-variants/sample_submission.csv"
+    ),
+)
+
+
+if os.getenv("LOCAL_ENVIRONMENT"):
+    config = config_local_env
+    sys.path.append(str(config.evaluation_api_path))
+    import kaggle_evaluation.mcts_inference_server  # type: ignore
+else:
+    config = config_kaggle_env
 
 
 def predict(test: pl.DataFrame, submission: pl.DataFrame) -> pl.DataFrame:
-    processor = Preprocessor.load(config.processor_path)
-    processor.disable_feature_store()
-    X = processor.transform(test)
+    preds = []
+    for dir_path in glob.glob(str(config.dataset_dir / "fold_*")):
+        # process test data
+        processor = Preprocessor.load(f"{dir_path}/processor.pickle")
+        processor.disable_feature_store()
+        X = processor.transform(test)
 
-    models = load_models(config.model_dir)
-    pred = predict_models(models, X)
-    pred = postprocess(pred)
+        # predict
+        model = ModelFactory.load(f"{dir_path}/model.pickle")
+        pred = model.predict(X)
+        pred = postprocess(pred)
 
-    return submission.with_columns(pl.Series("utility_agent1", pred))
+        preds.append(pred)
+
+    prediction = np.stack(preds).mean(axis=0)
+
+    return submission.with_columns(pl.Series("utility_agent1", prediction))
 
 
 def main() -> None:
