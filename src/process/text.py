b5c760582828bbc1e3675035fb292d42d75228e6
mysuikabar <mizuho.suikabar@gmail.com>
2024-10-04 14:27:47 +0000

    feat: update text_preprocess
    - remove stop words

--- src/process/text.py
+++ src/process/text.py
@@ -1,10 +1,20 @@
 import re
 import string
 
+from .consts import STOP_WORDS
+
 
 def preprocess_text(text: str) -> str:
     text = text.lower()
-    text = text.translate(str.maketrans("", "", string.punctuation))
+
+    # replace punctuation with space
+    translator = str.maketrans(string.punctuation, " " * len(string.punctuation))
+    text = text.translate(translator)
+
+    # remove extra spaces
     text = re.sub(r"\s+", " ", text).strip()
+
+    # remove stopwords
+    text = " ".join([word for word in text.split() if word not in STOP_WORDS])
 
     return text
