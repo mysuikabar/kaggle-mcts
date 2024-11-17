7bf8aed62cdb5ec439e6c3ae05c78a518e51e07e
mysuikabar <mizuho.suikabar@gmail.com>
2024-11-17 07:35:48 +0000

    feat: update nn module

--- src/model/nn/model.py
+++ src/model/nn/model.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import pytorch_lightning as pl
 import torch
+import yaml
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from torch.utils.data import DataLoader
 from typing_extensions import Self
@@ -11,7 +12,6 @@ from typing_extensions import Self
 from ..base import BaseModel
 from .dataset import MCTSDataModule, MCTSDataset
 from .module import NNModule
-from .processor import Preprocessor
 
 NUM_WORKERS = 0  # os.cpu_count()
 
@@ -19,6 +19,7 @@ NUM_WORKERS = 0  # os.cpu_count()
 class NNModel(BaseModel):
     def __init__(
         self,
+        num_numerical_features: int,
         categorical_feature_dims: dict[str, int],
         embedding_dim: int,
         hidden_dims: list[int],
@@ -29,24 +30,24 @@ class NNModel(BaseModel):
         early_stopping_patience: int,
         batch_size: int,
     ) -> None:
-        self._model: NNModule | None = None
-        self._processor = Preprocessor()
-        self._categorical_feature_dims = categorical_feature_dims.copy()
-        self._categorical_features = list(categorical_feature_dims.keys())
-        self._embedding_dim = embedding_dim
-        self._hidden_dims = hidden_dims
-        self._dropout_rate = dropout_rate
-        self._learning_rate = learning_rate
-        self._scheduler_patience = scheduler_patience
+        self._model_config = {
+            "num_numerical_features": num_numerical_features,
+            "categorical_feature_dims": categorical_feature_dims,
+            "embedding_dim": embedding_dim,
+            "hidden_dims": hidden_dims,
+            "dropout_rate": dropout_rate,
+            "learning_rate": learning_rate,
+            "scheduler_patience": scheduler_patience,
+        }
         self._max_epochs = max_epochs
         self._early_stopping_patience = early_stopping_patience
         self._batch_size = batch_size
 
+        self._model = NNModule(**self._model_config)  # type: ignore
+        self._categorical_features = list(categorical_feature_dims.keys())
+
     def fit(self, X_tr: pd.DataFrame, y_tr: np.ndarray, X_va: pd.DataFrame, y_va: np.ndarray) -> Self:
         # data
-        X_tr = self._processor.fit_transform(X_tr, self._categorical_feature_dims)
-        X_va = self._processor.transform(X_va)
-
         self._data_module = MCTSDataModule(
             X_tr=X_tr,
             X_va=X_va,
@@ -56,45 +57,18 @@ class NNModel(BaseModel):
             batch_size=self._batch_size,
         )
 
-        # model
-        self._model = NNModule(
-            num_numerical_features=X_tr.shape[1] - len(self._categorical_features),
-            categorical_feature_dims=self._categorical_feature_dims,
-            embedding_dim=self._embedding_dim,
-            hidden_dims=self._hidden_dims,
-            dropout_rate=self._dropout_rate,
-            learning_rate=self._learning_rate,
-            scheduler_patience=self._scheduler_patience,
-        )
-
         # training
         early_stop_callback = EarlyStopping(monitor="val_loss", patience=self._early_stopping_patience)
         checkpoint_callback = ModelCheckpoint(monitor="val_loss", save_top_k=1, mode="min")
-        self._trainer = pl.Trainer(
-            max_epochs=self._max_epochs,
-            callbacks=[early_stop_callback, checkpoint_callback],
-        )
+        self._trainer = pl.Trainer(max_epochs=self._max_epochs, callbacks=[early_stop_callback, checkpoint_callback])
         self._trainer.fit(self._model, datamodule=self._data_module)
 
         # load best model
-        self._model = NNModule.load_from_checkpoint(
-            checkpoint_callback.best_model_path,
-            num_numerical_features=X_tr.shape[1] - len(self._categorical_features),
-            categorical_feature_dims=self._categorical_feature_dims,
-            embedding_dim=self._embedding_dim,
-            hidden_dims=self._hidden_dims,
-            dropout_rate=self._dropout_rate,
-            learning_rate=self._learning_rate,
-            scheduler_patience=self._scheduler_patience,
-        ).to("cpu")
+        self._model = NNModule.load_from_checkpoint(checkpoint_callback.best_model_path, **self._model_config).to("cpu")  # type: ignore
 
         return self
 
     def predict(self, X: pd.DataFrame) -> np.ndarray:
-        if self._model is None:
-            raise RuntimeError("Model must be fitted before prediction")
-
-        X = self._processor.transform(X)
         dataset = MCTSDataset(X, self._categorical_features)
         dataloader = DataLoader(dataset, batch_size=self._batch_size, num_workers=NUM_WORKERS)
 
@@ -110,9 +84,21 @@ class NNModel(BaseModel):
         return torch.cat(preds).numpy()
 
     def save(self, filepath: str | Path) -> None:
-        if self._model is None:
-            raise RuntimeError("Model must be fitted before saving")
-        torch.save(self._model.state_dict(), filepath)
+        filepath = Path(filepath)
+        filepath.mkdir(parents=True, exist_ok=True)
+
+        torch.save(self._model.state_dict(), filepath / "model.pth")
+        with open(filepath / "model_config.yaml", "w") as f:
+            yaml.dump(self._model_config, f)
 
     def load(self, filepath: str | Path) -> Self:  # type: ignore
-        raise NotImplementedError  # TODO: implement
+        filepath = Path(filepath)
+
+        with open(filepath / "model_config.yaml", "r") as f:
+            self._model_config = yaml.safe_load(f)
+
+        self._model = NNModule(**self._model_config)  # type: ignore
+        self._model.load_state_dict(torch.load(filepath / "model.pth"))
+        self._model.eval()
+
+        return self
